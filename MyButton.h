7ab78634ddf6eb6004ce3a2cab75c7d64f1c85a8
Hanzeel Villa <hanzeelvilla0@gmail.com>
2025-04-06 20:39:44 -0600

    author

--- MyButton.h
+++ MyButton.h
@@ -1,6 +1,13 @@
 #ifndef MyButton_H
 #define MyButton_H
 
+/*
+  MyButton.h - A small and simple Arduino library to handle physical buttons
+  Created by Hanzeel Villa, April 6, 2025.
+  Released into the public domain.
+
+*/
+
 #include <Arduino.h>
 
 class MyButton {
